851da059a4b9320453b69dbe8834b0141f423f84
SaiKisho <diegosferas@gmail.com>
2023-08-26 02:43:21 -0500

    ♻️ Removed Unused code

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -1,7 +1,3 @@
 export const removeOverflowInBody = () => {
     document.getElementsByTagName('body')[0].classList.remove('overflow-hidden');
 }
-
-export const removeBgColorInBody = () => {
-    document.getElementsByTagName('main')[0].classList.remove('bg-[--bg-color]');
-}
