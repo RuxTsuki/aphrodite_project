8d3b94e95ea19f7b24a1a1f2efcecc494965c8af
SaiKisho <diegosferas@gmail.com>
2023-09-02 16:29:46 -0500

    ✨  Login View, not ready yet

---
+++ src/ui/molecules/auth/Login/Login.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import { Button, Input } from '@nextui-org/react'
+import React from 'react'
+
+export const Login = () => {
+    return (
+        <div className="container max-w-[700px] px-[50px] mt-[50px]">
+            <h3 className='title'>Iniciar Sesión</h3>
+
+            <div className="mt-[50px]">
+                <form action="" className="flex flex-col gap-[10px] place-content-center">
+                    <Input size={'sm'} type="email" placeholder="Enter your email" />
+                    <Input size={'sm'} type="password" placeholder="Enter your Password" />
+
+                    <p className="normal-text mt-[5px]"> ¿Olvidaste tu
+                        <span className='text-main underline pl-1'>
+                            Contraseña?
+                        </span>
+                    </p>
+
+                    <Button color="primary" className="w-full mt-[10px] px-[20px] py-[8px] text-[12px] h-auto rounded">
+                        Iniciar Sesion
+                    </Button>
+
+                    <p className="normal-text mt-[10px]">¿No tienes cuenta?
+                        <span className='text-main underline pl-1'>Registrate</span>
+                    </p>
+
+                </form>
+            </div>
+        </div>
+    )
+}
