85483aba2171cc68b1242d1f7aa60579671e75b1
SaiKisho <diegosferas@gmail.com>
2023-09-02 17:21:13 -0500

    ♻️  Navigation & style

--- src/ui/molecules/auth/Login/Login.tsx
+++ src/ui/molecules/auth/Login/Login.tsx
@@ -1,11 +1,17 @@
 'use client'
 
 import { Button, Input } from '@nextui-org/react'
-import React from 'react'
+import { useRouter } from "next/navigation"
 
 export const Login = () => {
+    const router = useRouter();
+
+    const goToCreateAccount = () => {
+        router.replace('/auth/register');
+    }
+
     return (
-        <div className="container max-w-[700px] px-[50px] mt-[50px]">
+        <div className="container max-w-[700px] px-[50px] mt-[50px] mx-auto">
             <h3 className='title'>Iniciar Sesión</h3>
 
             <div className="mt-[50px]">
@@ -24,7 +30,7 @@ export const Login = () => {
                     </Button>
 
                     <p className="normal-text mt-[10px]">¿No tienes cuenta?
-                        <span className='text-main underline pl-1'>Registrate</span>
+                        <span onClick={() => goToCreateAccount()} className='text-main underline pl-1'>Registrate</span>
                     </p>
 
                 </form>
