65f7c0df02310b106f2d296eecbb2d3346e19945
SaiKisho <diegosferas@gmail.com>
2023-09-02 16:26:30 -0500

    💄 Styles, changed variables & some adjust to class

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,6 +17,10 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        main: "#ff4ecd",
+        primary: "#ff4ecd"
+      }
     },
   },
   plugins: [nextui()],
