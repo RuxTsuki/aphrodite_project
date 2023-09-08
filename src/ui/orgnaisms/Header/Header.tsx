700bc1dd11d72d4e087f853e46cd1000c2591f36
SaiKisho <diegosferas@gmail.com>
2023-09-07 23:36:59 -0500

    ✨  Sidebar menu adjust
    ~ header now listen for changes in sidebar menu
    ~ sidebar menu is almost ready

--- src/ui/orgnaisms/Header/Header.tsx
+++ src/ui/orgnaisms/Header/Header.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useAppDispatch } from "@/hooks/state/useStateTypes"
+import { useAppDispatch, useAppSelector } from "@/hooks/state/useStateTypes"
 import { toggleMenu } from "@/store/reducers/sideBarMenu/sideBarMenu.slice"
 import { AphroditeLogo, Menu } from "@/ui/atoms"
 import { Button } from "@nextui-org/react"
@@ -8,6 +8,7 @@ import Link from "next/link"
 
 export const Header = () => {
 
+    const { isShrink } = useAppSelector(state => state.sideBarMenu)
     const dispatch = useAppDispatch();
 
     const toggleSideBarMenu = () => {
@@ -18,12 +19,14 @@ export const Header = () => {
         /* el height de este div toca cambiarlo */
         <div className="relative h-[--height-menu]">
             <nav className='fixed top-0 left-0 right-0 z-[20] px-[18px] py-[14px] flex items-center justify-between bg-[--bg-color]'>
-                <div className="flex gap-[10px] items-center justify-center">
+                <div className={`flex gap-[10px] items-center justify-center`}>
                     <Button onClick={() => toggleSideBarMenu()} size="sm" variant="light">
                         <Menu className="general-icon-color" />
                     </Button>
 
-                    <AphroditeLogo />
+                    <span className={`${isShrink ? 'ml-[28px]' : ''}`}>
+                        <AphroditeLogo />
+                    </span>
                 </div>
 
                 <Button color="primary" className="px-[20px] py-[8px] text-[12px] h-auto w-auto rounded">
--- src/ui/orgnaisms/SideMenu/SideMenu.tsx
+++ src/ui/orgnaisms/SideMenu/SideMenu.tsx
@@ -3,14 +3,21 @@
 import './side_menu.css';
 import { Button } from '@nextui-org/react';
 import { useAppDispatch, useAppSelector } from '@/hooks/state/useStateTypes';
-import { toggleShrink } from '@/store/reducers/sideBarMenu/sideBarMenu.slice';
+import { toggleMenu, toggleShrink } from '@/store/reducers/sideBarMenu/sideBarMenu.slice';
+import { AphroditeLogo, Close, LeftArrowThick, RightArrowThick, ToggleTheme } from '@/ui/atoms';
+import Link from 'next/link';
 
 export const SideMenu = () => {
     const { isOpen, isShrink } = useAppSelector(state => state.sideBarMenu);
     const dispatch = useAppDispatch();
 
-    const toggleSmallView = () => {
-        dispatch(toggleShrink(!isShrink));
+    const toggleSmallView = (value: boolean) => {
+        dispatch(toggleShrink(value));
+        document.getElementsByClassName('main-content')[0].classList.toggle('main-small-shrink');
+    }
+
+    const toggleMenuFromOutside = () => {
+        dispatch(toggleMenu(!isOpen));
     }
 
     const sideMenus = [{
@@ -27,11 +34,18 @@ export const SideMenu = () => {
 
     return (
         <>
-            <div className={`${isOpen ? 'side-bar-open ' : ''}${isShrink ? 'small-side-menu ' : ''}side-bar-container fixed z-[22] left-[-250px] top-0 md:flex flex-col justify-between overflow-hidden w-[250px] h-full py-[1.25rem] px-[1rem] bg-[--bg-color-side-menu]`}>
-                <div className='side-bar-top w-full h-[45px] flex items-center'>
-                    <Button onClick={() => toggleSmallView()} color="primary" className="px-[20px] py-[8px] text-[12px] h-auto w-auto rounded">
-                        Open
+            <div className={`${isOpen ? 'side-bar-open ' : ''}${isShrink ? 'small-side-menu ' : ''}side-bar-container fixed z-[25] top-0  flex-col justify-between overflow-hidden w-[--side-bar-menu-left] h-full py-[1.25rem] px-[1rem] bg-[--bg-color-side-menu]`}>
+                <div className={`left-[231px] ${isShrink ? 'left-[60px]' : ''} ${isOpen ? 'block ' : 'hidden '}side-bar-arrows-view md:hidden fixed rounded-full p-[6px] bg-[--bg-color]`}>
+                    <LeftArrowThick onClick={() => toggleSmallView(true)} className={`${isShrink ? 'hidden ' : 'block '}general-icon-color`} />
+                    <RightArrowThick onClick={() => toggleSmallView(false)} className={`${isShrink ? 'block ' : 'hidden '}general-icon-color`} />
+                </div>
+
+                <div className='side-bar-top w-full h-[45px] flex items-center gap-2'>
+                    <Button onClick={() => toggleMenuFromOutside()} isIconOnly variant='light' className="px-[2px] h-auto w-auto rounded">
+                        <Close className='general-icon-color' />
                     </Button>
+
+                    <AphroditeLogo />
                 </div>
 
                 <nav className='side-bar-nav h-full overflow-y-auto overflow-x-hidden'>
@@ -57,7 +71,7 @@ export const SideMenu = () => {
                     <div className='line mt-[1rem]'></div>
 
                     <div className='change-theme'>
-
+                        <ToggleTheme />
                     </div>
 
                     <div className='user'>
@@ -65,6 +79,11 @@ export const SideMenu = () => {
                     </div>
                 </div>
             </div>
+
+            <div
+                onClick={() => toggleMenuFromOutside()}
+                className={`${isOpen && !isShrink ? 'block ' : 'hidden '}
+            absolute bg-darkness-screen top-0 bottom-0 left-0 z-[22] right-0 h-full w-full`}></div>
         </>
     )
 }
