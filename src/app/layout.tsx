ecc8bd3b1274f93ee13ed4e81066725e0b3c22b2
SaiKisho <diegosferas@gmail.com>
2023-10-28 16:10:32 -0500

    ♻️  | Meta themeColor moved

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,11 +4,14 @@ import type { Metadata } from 'next';
 import { ThemeProvider } from '@/theme/Provider';
 import { outfit, playball } from './fonts';
 
-
 export const metadata: Metadata = {
   title: 'Aphrodite Project',
   description: 'Aphrodite Project es una innovadora app para reservación de citas para peluquerías y gestión completa de negocios del sector de la belleza.',
   manifest: '/manifest.json',
+
+}
+
+export const viewport = {
   themeColor: '#000',
 }
 
