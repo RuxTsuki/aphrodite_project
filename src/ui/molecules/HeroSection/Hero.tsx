6384b893532f1c38a231272d5971bcd47c2f3ec8
SaiKisho <diegosferas@gmail.com>
2023-11-03 23:37:58 -0500

    🚧  | Searchbar in progress not ready

--- src/ui/molecules/HeroSection/Hero.tsx
+++ src/ui/molecules/HeroSection/Hero.tsx
@@ -5,9 +5,6 @@ import { SearchSection } from '@/ui/molecules';
 export const HeroSection = () => {
     return (
         <div className="w-full h-[186px] grid place-items-center">
-            <h2 className="title">
-                Que Servicio Buscas ?
-            </h2>
 
             <SearchSection />
         </div>
--- src/ui/molecules/SearchSection/Services/Services.tsx
+++ src/ui/molecules/SearchSection/Services/Services.tsx
@@ -2,19 +2,42 @@
 
 import { servicesForUsers } from "@/mocks/services";
 import { Search } from "@/ui/atoms";
-import { Select, SelectItem, Chip, Button } from "@nextui-org/react";
+import { Select, SelectItem, Chip, Button, Selection } from "@nextui-org/react";
+import { ChangeEvent, ChangeEventHandler, useState } from "react";
 
 
 export const Services = () => {
+
+    const [allServices, setAllServices] = useState<Selection>(new Set([]));
 
     const onSearch = () => {
 
     }
 
+    const handleClose = (fruitToRemove: string | number | null) => {
+        if (!fruitToRemove) return;
+
+        /*         const newItemsSelected = allServices.
+                    setAllServices(allServices.map(fruit => ({
+                        ...fruit,
+        
+                    })));
+                if (allServices.length === 1) {
+                    setAllServices([...servicesForUsers]);
+                } */
+
+        /* console.log(fruitToRemove) */
+    };
+
+    const handleSelectionChange = (e: ChangeEvent<HTMLSelectElement>) => {
+        setAllServices(new Set(e.target.value.split(",")));
+    };
+
     return (
         <div className="flex items-center gap-2 pt-[10px]">
             <Select
-                items={servicesForUsers}
+                selectedKeys={allServices}
+                onChange={handleSelectionChange}
                 variant="bordered"
                 isMultiline={true}
                 size="lg"
@@ -26,31 +49,40 @@ export const Services = () => {
                     trigger: "min-h-unit-12 py-2",
                 }}
                 renderValue={(items) => {
+                    console.log(items)
                     return (
                         <div className="flex flex-wrap gap-2">
                             {items.map((item) => (
-                                <Chip variant="flat"
+                                <Chip
+                                    size="lg"
+                                    variant="flat"
                                     color="secondary"
-                                    key={item.key}>{item.data?.name}</Chip>
+                                    key={item.key}
+                                    onClose={() => {
+                                        handleClose(item.key || null);
+                                    }}
+                                >{item.textValue}</Chip>
                             ))}
                         </div>
                     );
                 }}
             >
-                {(service) => (
-                    <SelectItem key={service.id} textValue={service.name}>
-                        <div className="flex gap-2 items-center">
-                            <div className="flex flex-col">
-                                <span className="text-small">{service.name}</span>
+                {
+                    servicesForUsers.map((service) => (
+                        <SelectItem key={service.id} textValue={service.name}>
+                            <div className="flex gap-2 items-center">
+                                <div className="flex flex-col">
+                                    <span className="text-small">{service.name}</span>
+                                </div>
                             </div>
-                        </div>
-                    </SelectItem>
-                )}
+                        </SelectItem>
+                    ))
+                }
             </Select>
 
-            <Button isIconOnly onClick={() => onSearch()} size="md" variant="light">
+            {/*             <Button isIconOnly onClick={() => onSearch()} size="md" variant="light">
                 <Search className="general-icon-color" />
-            </Button>
+            </Button> */}
         </div>
     )
 }
