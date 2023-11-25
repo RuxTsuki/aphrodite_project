fe0d60b8bc97b7794ce3193d88a8334f394a5804
SaiKisho <diegosferas@gmail.com>
2023-11-25 14:34:25 -0500

    🚧  | route utils

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -3,3 +3,4 @@ export const viewTourLocalS = 'tourViewed';
 export const LoginRoute = '/join';
 export const RegisterRoute = '/join/register';
 export const RecoverRoute = '/join/recover';
+export const HairSalonsRoute = '/hairsalons';
